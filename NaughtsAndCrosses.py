66bceda555b1e074dc2ba1d109aabe181f37d635
AndrewTHowell <howelldrew99@gmail.com>
2020-06-12 22:00:28 +0100

    Play loop nearly done

--- NaughtsAndCrosses.py
+++ NaughtsAndCrosses.py
@@ -5,13 +5,14 @@ class Board:
 
     def __init__(self, boardSize=3):
         self.boardSize = boardSize
-        self.board = np.zeros((self.boardSize, self.boardSize), dtype=int)
-
         self.players = {
             0: " ",
             1: "O",
             2: "X"
         }
+
+        self.board = np.zeros((self.boardSize, self.boardSize), dtype=int)
+        self.player = 1
 
     def __str__(self):
         endCharacters = " |"
@@ -22,12 +23,23 @@ class Board:
             for cell in row:
                 string += " " + self.players[cell] + endCharacters
             string = string[:-len(endCharacters)] + "\n"
+        return string[:-2]  # Remove trailing \n
+
+    @property
+    def won(self):
+        # TODO - check for win state
+        return False
 
         return string
 
     def play(self):
         # Reset the board
         self.board = np.zeros((self.boardSize, self.boardSize), dtype=int)
+        self.player = 1
+
+        # while not self.won:
+        for i in range(20):
+            self.nextMove()
 
 
 if __name__ == "__main__":
