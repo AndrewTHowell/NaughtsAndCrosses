5b601b5445cd0daa495d269642f0e8c4f8622323
AndrewTHowell <howelldrew99@gmail.com>
2020-06-12 22:20:06 +0100

    All game mechanics sorted, other than win condition

--- NaughtsAndCrosses.py
+++ NaughtsAndCrosses.py
@@ -24,33 +24,53 @@ class Board:
             for cell in row:
                 string += " " + self.players[cell] + endCharacters
             string = string[:-len(endCharacters)] + "\n"
-        return string[:-2]  # Remove trailing \n
+        return string[:-1]  # Remove trailing \n
 
     @property
     def won(self):
         # TODO - check for win state
         return False
 
-    def nextPlayer(self):
-        self.player = (self.player % (len(self.players) - 1)) + 1
+    def changePlayer(self, num=1):
+        self.player = ((self.player - 1 - num) % (len(self.players) - 1)) + 1
+
+    def attemptMove(self, row, col):
+        # Decrement numbers to conform to array indices
+        row, col = row - 1, col - 1
+
+        if self.board[row][col] == 0:
+            self.board[row][col] = self.player
+            return True
+        return False
 
     def nextMove(self):
         print(f"\nPlayer {self.player}'s Turn:")
         print(f"\nWhere do you want to place an {self.players[self.player]}")
         print(self)
-        rowNum = UI.getNum("Row Num", 1, self.boardSize)
-        colNum = UI.getNum("Column Num", 1, self.boardSize)
 
-        self.nextPlayer()
+        validMove = None
+        while not validMove:
+            if validMove is not None:
+                print("Invalid Move")
+            rowNum = UI.getNum("Row Num", 1, self.boardSize)
+            colNum = UI.getNum("Column Num", 1, self.boardSize)
+
+            validMove = self.attemptMove(rowNum, colNum)
+
+        self.changePlayer()
 
     def play(self):
         # Reset the board
         self.board = np.zeros((self.boardSize, self.boardSize), dtype=int)
         self.player = 1
 
-        # while not self.won:
-        for i in range(20):
+        while not self.won:
             self.nextMove()
+
+        # Undo moving to player after winning player
+        self.changePlayer(num=-1)
+
+        print(f"Player {self.player} won!!")
 
 
 if __name__ == "__main__":
